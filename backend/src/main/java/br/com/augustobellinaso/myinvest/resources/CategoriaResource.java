8d8f1d796775fcef482a03fca157798ecbeee50b
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-05-24 14:28:33 -0300

    Configurando os recursos/controllers

---
+++ backend/src/main/java/br/com/augustobellinaso/myinvest/resources/CategoriaResource.java
@@ -0,0 +1,7 @@
+package br.com.augustobellinaso.myinvest.resources;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CategoriaResource {
+}
---
+++ backend/src/main/java/br/com/augustobellinaso/myinvest/resources/InvestimentoResource.java
@@ -0,0 +1,36 @@
+package br.com.augustobellinaso.myinvest.resources;
+
+import br.com.augustobellinaso.myinvest.domain.Investimento;
+import br.com.augustobellinaso.myinvest.repository.InvestimentoRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/investimentos")
+@AllArgsConstructor
+public class InvestimentoResource {
+
+    private InvestimentoRepository investimentoRepository;
+
+    @GetMapping
+    public List<Investimento> listarTodos() {
+        return investimentoRepository.findAll();
+    }
+
+    @GetMapping("/{codigo}")
+    public Investimento buscarPeloCodigo(@PathVariable Long codigo) {
+        return investimentoRepository.findById(codigo).orElse(null);
+    }
+
+    @DeleteMapping("/{codigo}")
+    public void remover(@PathVariable Long codigo) {
+        investimentoRepository.deleteById(codigo);
+    }
+
+    @PostMapping
+    public Investimento cadastrar(@RequestBody Investimento investimento){
+        return investimentoRepository.save(investimento);
+    }
+}
