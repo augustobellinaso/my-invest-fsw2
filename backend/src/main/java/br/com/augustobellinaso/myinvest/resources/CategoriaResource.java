4fb1f33c5390666689e3815741e47427c27b5d2c
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-05-29 13:53:09 -0300

    Configurando o CORS e criando o controller de categoria

--- backend/src/main/java/br/com/augustobellinaso/myinvest/resources/CategoriaResource.java
+++ backend/src/main/java/br/com/augustobellinaso/myinvest/resources/CategoriaResource.java
@@ -1,7 +1,37 @@
 package br.com.augustobellinaso.myinvest.resources;
 
-import org.springframework.web.bind.annotation.RestController;
+import br.com.augustobellinaso.myinvest.domain.Categoria;
+import br.com.augustobellinaso.myinvest.repository.CategoriaRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@AllArgsConstructor
+@RequestMapping("/categorias")
+@CrossOrigin(origins = "http://localhost:3000")
 public class CategoriaResource {
+
+    private CategoriaRepository categoriaRepository;
+
+    @GetMapping
+    public List<Categoria> listarTodas() {
+        return categoriaRepository.findAll();
+    }
+
+    @GetMapping("/{codigo}")
+    public Categoria buscarPeloCodigo(@PathVariable Long codigo){
+        return categoriaRepository.findById(codigo).orElse(null);
+    }
+
+    @DeleteMapping("/{codigo}")
+    public void remover(@PathVariable Long codigo) {
+        categoriaRepository.deleteById(codigo);
+    }
+
+    @PostMapping
+    public Categoria criar(@RequestBody Categoria categoria){
+        return categoriaRepository.save(categoria);
+    }
 }
